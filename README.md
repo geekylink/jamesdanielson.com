95cbb6baee09ce124509d8461598f6e1e411b23f
James Danielson <geekylink@github.io>
2023-01-24 17:38:25 -0500

    removed some worthless stuff, added a websites page and license file

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ npm run dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-**Build Static Site:**
+## Build Static Site:
 
 ```bash
 npm run build
--- src/pages/websites.tsx
+++ src/pages/websites.tsx
@@ -29,11 +29,48 @@ const What: NextPage = () => {
 
         <div className={styles.textsection}>
           <p className={styles.text}>
-            <strong><Link href="/games/merge">M.E.R.G.E.</Link></strong><br /><br />
-            Simple cooperative unity game similar to asteroids, players must work together to defeat larger enemies by combining ships together.<br /><br />
-
+            <strong><Link href="https://gekinzuku.com" target="_blank" rel="noopener noreferrer">Gekinzuku</Link></strong><br /><br />
+            Old homebrew game and game modding community. Largely focused on modding the Nintendo 64 Legend of Zelda games. Active from 2008 - 2011.<br /><br />
+            Used to feature a forum, small IRC network (Three Linux servers: Michigan, California, and London with a simple round-robin load balancer)<br /><br />
+            <strong>Note:</strong> Has been dead for about ten years.
+            <br /><br />
           </p>
         </div>
+
+        <div className={styles.textsection}>
+          <p className={styles.text}>
+            <strong><Link href="https://toothtreks.com" target="blank" rel="noopener noreferrer">Tooth Treks</Link></strong><br /><br />
+            A website of mine for helping people from the USA &amp; Canada find
+            affordable dental care abroad in Latin America and enjoy a dream
+            getaway too. Many major dental operations are unaffordable back
+            home, but using my substantial experience abroad, I can help find
+            the service you need at a much more reasonable price.
+            <br /><br />
+          </p>
+        </div>
+
+        <div className={styles.textsection}>
+          <p className={styles.text}>
+            <strong><Link href="https://jamesdanielson.dev" target="blank" rel="noopener noreferrer">James Danielson Development</Link></strong><br /><br />
+            Another site of mine, mostly used for development tests. Homepage serves as a freelance hire hub for development work.
+            <br /><br />
+          </p>
+        </div>
+
+
+        {<div className={styles.textsection}>
+          <p className={styles.text}>
+            <strong><Link href="https://expatcitizen.com" target="blank" rel="noopener noreferrer">Expat Citizen</Link></strong><br /><br />
+            An infrequently updated blog about oppurtunities abroad I have
+            learned about. From my significant time abroad and meeting with
+            many different people, I have learned a number of interesting
+            tips and tricks. Blog is largely focused on residency abroad and
+            some investment options to help diversify into the international 
+            space.
+            <br /><br />
+          </p>
+  </div>}
+
 
         <div className={styles.grid}>
           <Link
